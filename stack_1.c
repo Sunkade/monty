93662f48f643fe6f317386ba94bb8843d9f8bfe2
Sunkade <sunkadefasola@gmail.com>
2023-08-27 10:26:12 -0700

    functions

---
+++ stack_1.c
@@ -0,0 +1,16 @@
+#include "monty.h"
+
+/**
+ * f_stack - prints the top
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+ */
+void f_stack(stack_t **head, unsigned int counter)
+{
+    (void)head;
+    (void)counter;
+    /* Assuming bus and lifi are global variables or structs defined elsewhere */
+    bus.lifi = 0;
+}
+
